6d0c571fa3215d5306764bc309f32e5768ea8b8b
wilson <wjwebdevelopments@outlook.com>
2021-04-17 12:00:32 -0400

    Agregada la seccion de conciertos

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,44 @@
-const { series, src, dest } = require('gulp');
+const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const paths = require('./paths');
 
-// Funcion que compila SASS
+
 function css() {
-    return src("./src/scss/app.scss")
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-        .pipe(sourcemaps.write())
+    return src(paths.scss)
+        .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(dest("./build/css"));
 }
 
+function minificarCss() {
+    return src(paths.scss)
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(dest("./build/css"));
+}
+
+function minificarImagenes() {
+    return src(paths.imagenes)
+        .pipe(imagemin())
+        .pipe(dest("./build/img"))
+        // .pipe(notify({ message: 'Imagen minificada' }));
+}
+
+function watchArchivo() {
+    // @args Archivo que se va a observar, tarea a ejecutar
+    watch(paths.scss, css); // *.scss = carpeta actual, **/*.scss = Todos los archivos dentro de la carpeta SCSS
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+}
+
+
 exports.css = css;
+exports.minificarImagenes = minificarImagenes;
+exports.minificarCss = minificarCss;
+exports.watchArchivo = watchArchivo;
+exports.default = series(css, minificarImagenes, versionWebp, watchArchivo);
---
+++ paths.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    // Directorio de imagenes
+    imagenes: './src/img/**/*',
+
+    // Directorio de archivos SCSS
+    scss: './src/scss/**/*.scss'
+
+
+}
